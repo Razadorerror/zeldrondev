1a728f03364e16729095991bb2a82f13787669f9
Razadorerror <85961110+Razadorerror@users.noreply.github.com>
2021-09-16 13:04:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+const client = new bot();
+const  bot  = require('discord.js');
+
+
+
+
+const talkedRecently = new Set();
+
+bot.on('ready', () => {
+  
+  console.log('Razador {EKAV BOT READY}')
+
+}
+)
+ 
+
+bot.login(process.env.token);
